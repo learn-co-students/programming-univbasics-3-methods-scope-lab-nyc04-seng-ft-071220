a8ccc62b88e1fed9229dff2b0aa4f752aeeaf227
hortencia718 <hortenciaecisneros@gmail.com>
2020-08-23 19:41:16 -0400

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,21 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  phrase = "It's-a me, Mario!"
   puts phrase
 end
+# call the method "mario and puts phrase to print out what mario says"
 
 def toadstool
+  status = "Thank You Mario! But Our Princess Is In Another Castle!"
   puts status
 end
+# just "puts out the methos toadstool tp puts "
+def link 
+  puts "It's Dangerous To Go Alone! Take This."
+end
+# this made no sense , it was usper unclear ... 
+def all_phrases
+  mario
+  toadstool
+  link
+end
+# this is just alling al the methods ! go it . made sense to me !
